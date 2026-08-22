075aeb51ea68fa863278106f1d77e7b90006d02e
Devdeepakjha <dj145740@gmail.com>
2026-08-22 15:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (53.09%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,14 @@
+// simple classic prefix_sum printing
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix_sum(n);
+        prefix_sum[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            prefix_sum[i] = prefix_sum[i-1] + nums[i];
+        }
+        return prefix_sum;
+    }
+};
