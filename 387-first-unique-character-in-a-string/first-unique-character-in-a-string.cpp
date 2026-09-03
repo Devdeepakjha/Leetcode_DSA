89f5f39a6394b0a52ad45f02cba97e1e82b2c8d9
Devdeepakjha <dj145740@gmail.com>
2026-09-03 23:10:30 +0530

    Time: 16 ms (30.06%) | Memory: 15.1 MB (7.00%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
             freq[x]++;
         }
         for(int i=0; i<s.size(); i++){
-            if(freq[s[i]] == 1){
+            if(freq[s[i]] == 1){ // not checking for freq[i] only bcz i have not created hashmap for that i, I have made as per the s[i] so passing that to check the frequency...
                 return i;
                 break;
             }
