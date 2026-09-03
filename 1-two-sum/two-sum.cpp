7b216521b9024ee36cc2b921d372f626e23e4e3a
Devdeepakjha <dj145740@gmail.com>
2026-09-03 20:22:33 +0530

    Time: 3 ms (67.17%) | Memory: 14.8 MB (58.56%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,12 +1,33 @@
+
+//BRUTEFORCE
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {  
+//         for(int i = 0; i<nums.size(); i++){
+//             for(int j = i + 1; j<nums.size(); j++){
+//                 if(nums[i] + nums[j] == target)
+//                 return {i,j};
+//             }
+//         }
+//         return {}; //returning a empty vector if nothing above worked!(remember)
+//     }
+// };
+
+//OPTIMAL
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i<nums.size(); i++){
-            for(int j = i + 1; j<nums.size(); j++){
-                if(nums[i] + nums[j] == target)
-                return {i,j};
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            int need = target - nums[i];
+            if(mp.find(need) != mp.end()){ //checking for out of bound thing...
+                return {mp[need], i};
             }
+            mp[nums[i]] = i;  //seeting value for each key(as there index, to keep check of index also)
         }
-        return {}; //returning a empty vector if nothing above worked!(remember)
+        return {}; //returning a empty vector
     }
 };
+
+//so this map has for each, in each row
+// key(nums[i]) and value(index of nums[i] i.e. i)
