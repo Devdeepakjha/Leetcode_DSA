b89c83207b8958a25c8a6f83fbf085853458c9df
Devdeepakjha <dj145740@gmail.com>
2026-09-09 20:22:58 +0530

    Time: 1 ms (22.57%) | Memory: 8.7 MB (23.56%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> jewelSet;
+
+        for(char c : jewels) {
+            jewelSet.insert(c);
+        }
+
+        int count = 0;
+
+        for(char c : stones) {
+            if(jewelSet.count(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
